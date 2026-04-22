89fdc523c771213cb96d6dd72926945f11d31a10
killioin007-source <killioin.007@gmail.com>
2026-04-22 16:07:13 -0400

    fixed the code and added the next part

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,17 +1,34 @@
 package com.pluralsight;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        ArrayList <Inventory> inventory = new ArrayList<Inventory>();
+        inventory.add(new Inventory(1, "Water Bottle",2));
+        inventory.add(new Inventory(2, "Redbull",5));
+        inventory.add(new Inventory(3, "Celsius",5));
+        inventory.add(new Inventory(4, "Juice",8));
+        inventory.add(new Inventory(5, "Smoothie",8));
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        Inventory a = null;
+        System.out.println("ID \t Product Name \t \t Price:");
+
+        for (int i = 0; i < inventory.size(); i++) {
+            a = inventory.get(i);
+            System.out.printf("\n"+"%d \t %s \t \t %.2f",a.getId(),a.getName(),a.getPrice());
         }
+        System.out.println("\n");
+        System.out.println("What product would you like to search?");
+
+        Scanner input = new Scanner(System.in);
+        String userInput = input.nextLine();
+
+
     }
+
 }
+
+
+
